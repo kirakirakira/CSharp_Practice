b02453a8edd03f78defc129d1bd72382bbb1f032
Kira Hartlage <kira.hartlage@gmail.com>
2018-08-19 14:23:45 -0400

    * .gitignore: Practice commit from within MonoDevelop
    * CSharp_Practice.sln:
    * Program.cs:
    * CSharp_Practice.csproj:
    * AssemblyInfo.cs:

---
+++ CSharp_Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string thing = "movie";
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite " + thing + "?");
+            string favoriteThing = Console.ReadLine();
+
+            Console.WriteLine("My name is " + name + " and my favorite " + thing + " is " + favoriteThing + ".");
+        }
+    }
+}
